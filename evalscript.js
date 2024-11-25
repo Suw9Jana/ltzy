322e31e3286b3b0788f643fcfdfcc80b2451cb3b
Suw9Jana <444200816@student.ksu.edu.sa>
2024-11-25 23:18:34 +0300

    Update evalscript.js

--- evalscript.js
+++ evalscript.js
@@ -1,9 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-
-
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("evalForm");
     const orderSelect = document.getElementById("order");
